68cf959fb66c23da8d2f23b3fdb4a06512871f37
Michael Dougall <mdougall@atlassian.com>
2021-09-14 20:41:34 +1000

    fix: set cookie to never expire

--- lib/toggle-theme.tsx
+++ lib/toggle-theme.tsx
@@ -6,7 +6,7 @@ function toggleTheme() {
 
   setGlobalTheme(nextTheme);
 
-  document.cookie = `theme=${nextTheme};`;
+  document.cookie = `theme=${nextTheme};expires=Fri, 31 Dec 9999 23:59:59 GMT;`;
 }
 
 export default toggleTheme;
