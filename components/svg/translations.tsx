328b4defebab97da49b106b01fc47a87542313a7
DOUGES <laheen@gmail.com>
2022-11-10 12:09:10 +1100

    feat: adds svgs

---
+++ components/svg/translations.tsx
@@ -0,0 +1,133 @@
+import { token } from '@atlaskit/tokens';
+import * as React from 'react';
+
+const SvgComponent = () => (
+  <svg
+    viewBox="0 0 518 187"
+    xmlns="http://www.w3.org/2000/svg"
+    xmlSpace="preserve"
+    style={{
+      display: 'block',
+      margin: '0 auto',
+      maxWidth: 400,
+      fillRule: 'evenodd',
+      clipRule: 'evenodd',
+      strokeLinejoin: 'round',
+      strokeMiterlimit: 1.5,
+    }}>
+    <path
+      d="M164.32 41.08C164.32 18.407 145.913 0 123.24 0H41.08C18.407 0 0 18.407 0 41.08v82.16c0 22.673 18.407 41.08 41.08 41.08h82.16c22.673 0 41.08-18.407 41.08-41.08V41.08Z"
+      style={{
+        fill: token('color.background.subtleNeutral.resting'),
+      }}
+    />
+    <path
+      style={{
+        fill: token('color.text.mediumEmphasis'),
+        fillOpacity: 0,
+        stroke: token('color.text.mediumEmphasis'),
+        strokeWidth: 4,
+        strokeDasharray: '4,4',
+      }}
+      fill="none"
+      d="M24.017 35.857h90.499v93.297H24.017z"
+    />
+    <path
+      style={{
+        fill: token('color.text.mediumEmphasis'),
+        fillOpacity: 0,
+        stroke: token('color.text.mediumEmphasis'),
+        strokeWidth: 4,
+        strokeLinecap: 'round',
+      }}
+      fill="none"
+      d="M49.724 35.857h90.499v93.297H49.724z"
+    />
+    <path
+      d="M340.748 41.08c0-22.673-18.407-41.08-41.08-41.08h-82.16c-22.672 0-41.08 18.407-41.08 41.08v82.16c0 22.673 18.408 41.08 41.08 41.08h82.16c22.673 0 41.08-18.407 41.08-41.08V41.08Z"
+      style={{
+        fill: token('color.background.subtleNeutral.resting'),
+      }}
+    />
+    <path
+      style={{
+        fill: token('color.text.mediumEmphasis'),
+        fillOpacity: 0,
+        stroke: token('color.text.mediumEmphasis'),
+        strokeWidth: 4,
+      }}
+      fill="none"
+      d="M200.445 35.857h90.499v93.297h-90.499z"
+    />
+    <path
+      style={{
+        fill: token('color.text.mediumEmphasis'),
+        fillOpacity: 0,
+        stroke: token('color.text.mediumEmphasis'),
+        strokeWidth: 4,
+        strokeDasharray: '4,4',
+      }}
+      fill="none"
+      d="M226.152 35.857h90.499v93.297h-90.499z"
+    />
+    <path
+      d="M517.712 41.08c0-22.673-18.407-41.08-41.08-41.08h-82.16c-22.673 0-41.08 18.407-41.08 41.08v82.16c0 22.673 18.407 41.08 41.08 41.08h82.16c22.673 0 41.08-18.407 41.08-41.08V41.08Z"
+      style={{
+        fill: token('color.background.subtleNeutral.resting'),
+      }}
+    />
+    <path
+      style={{
+        fill: token('color.text.mediumEmphasis'),
+        fillOpacity: 0,
+        stroke: token('color.text.mediumEmphasis'),
+        strokeWidth: 4,
+      }}
+      fill="none"
+      d="M390.242 25.857h90.499v93.297h-90.499z"
+    />
+    <path
+      style={{
+        fill: token('color.text.mediumEmphasis'),
+        fillOpacity: 0,
+        stroke: token('color.text.mediumEmphasis'),
+        strokeWidth: 4,
+        strokeDasharray: '4,4',
+      }}
+      fill="none"
+      d="M390.242 45.857h90.499v93.297h-90.499z"
+    />
+    <text
+      x={39.368}
+      y={183.167}
+      style={{
+        fontFamily: "'Karla', sans-serif",
+        fontSize: 18,
+        fill: token('color.text.lowEmphasis'),
+      }}>
+      {'x += 1.0'}
+    </text>
+    <text
+      x={213.779}
+      y={183.167}
+      style={{
+        fontFamily: "'Karla', sans-serif",
+        fontSize: 18,
+        fill: token('color.text.lowEmphasis'),
+      }}>
+      {'x -= 1.0'}
+    </text>
+    <text
+      x={393.534}
+      y={183.167}
+      style={{
+        fontFamily: "'Karla', sans-serif",
+        fontSize: 18,
+        fill: token('color.text.lowEmphasis'),
+      }}>
+      {'y += 1.0'}
+    </text>
+  </svg>
+);
+
+export default SvgComponent;
